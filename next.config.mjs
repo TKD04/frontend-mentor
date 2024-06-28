19f63edafff71a20c068d3e0c1deb0613fff7aa1
TKD04 <tkd04fordev@outlook.com>
2024-06-28 15:26:14 +0900

    Remove config for production because `actions/configure-pages@v5` inject `basePath` automatically

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-const isProd =
-  typeof process !== "undefined" && process.env.NODE_ENV === "production";
-
-console.log(`isProd: ${isProd}`)
-console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
-
 /** @type {import('next').NextConfig} */
-const nextCofnig = isProd
-  ? {
-      basePath: "/frontend-mentor",
-      output: "export",
-    }
-  : {};
+const nextCofnig = {};
 
 export default nextCofnig;

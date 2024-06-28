a9bf8b44ea0b431445e74aa5d5d0df0e2da800f3
TKD04 <tkd04fordev@outlook.com>
2024-06-28 15:12:08 +0900

    Add `console.log` to debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-const isProd = process.env.NODE_ENV === "production";
+const isProd =
+  typeof process !== "undefined" && process.env.NODE_ENV === "production";
+
+console.log(`isProd: ${isProd}`)
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 
 /** @type {import('next').NextConfig} */
 const nextCofnig = isProd

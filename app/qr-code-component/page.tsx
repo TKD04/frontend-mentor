8a18d83ac55da9f300b51f401a0eafc32a4aadad
TKD04 <tkd04fordev@outlook.com>
2024-07-01 15:55:53 +0900

    Change `rounded-2xl` and `rounded-lg` to `rounded-3xl` and `rounded-xl to match the original design`

--- app/qr-code-component/page.tsx
+++ app/qr-code-component/page.tsx
@@ -18,14 +18,14 @@ export default function Page() {
     <main
       className={`${outfit.className} grid min-h-screen place-items-center bg-[var(--lightgray)] p-4 text-gray-800`}
     >
-      <div className="flex w-full max-w-80 flex-col gap-6 rounded-2xl bg-[var(--white)] p-4 shadow-[0_28px_32px_0_rgba(0,0,0,0.08)]">
+      <div className="flex w-full max-w-80 flex-col gap-6 rounded-3xl bg-[var(--white)] p-4 shadow-[0_28px_32px_0_rgba(0,0,0,0.08)]">
         <Image
           src={qrCodePic}
           alt="QR Code"
           loading="eager"
           placeholder="blur"
           priority
-          className="h-auto w-full rounded-lg"
+          className="h-auto w-full rounded-xl"
         />
         <div className="mb-6 flex flex-col gap-3 px-3 text-center">
           <h1 className="text-[1.4rem] font-bold leading-7 text-[var(--darkblue)]">

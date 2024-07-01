96536ab0d6727c43234980cf9de3fdc70f8a1844
TKD04 <tkd04fordev@outlook.com>
2024-07-01 14:57:03 +0900

    Use `font-medium` as default

--- app/blog-preview-card/page.tsx
+++ app/blog-preview-card/page.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main
-      className={`${figtree.className} grid min-h-screen place-items-center bg-[var(--yellow)] p-6 text-gray-800`}
+      className={`${figtree.className} grid min-h-screen place-items-center bg-[var(--yellow)] p-6 font-medium text-gray-800`}
     >
       <article className="pointer-events-none flex w-full max-w-96 flex-col gap-6 rounded-2xl border border-[var(--black)] bg-[var(--white)] p-6 shadow-[8px_8px_0_0_var(--black)] hover:shadow-[16px_16px_0_0_var(--black)]">
         <Image

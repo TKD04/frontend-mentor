cfa01f536c08887d38792733a72f48e1f7475fdb
TKD04 <tkd04fordev@outlook.com>
2024-08-29 16:51:20 +0900

    Fix some styles to match the design

--- app/four-card-feature-section/page.tsx
+++ app/four-card-feature-section/page.tsx
@@ -18,13 +18,13 @@ export default function Page() {
 
   return (
     <main
-      className={`${poppins.className} flex min-h-screen justify-center bg-[var(--very-light-gray)] p-8 pb-[4.8rem] pt-20 text-[var(--grayish-blue)] md:pt-[5.1rem]`}
+      className={`${poppins.className} flex min-h-screen justify-center bg-[var(--very-light-gray)] p-8 pb-[4.8rem] pt-20 text-[var(--grayish-blue)] md:pb-20`}
     >
       <article
-        className="flex w-full max-w-[68rem] flex-col items-center"
+        className="flex w-full max-w-[69rem] flex-col items-center"
         data-aos="fade-up"
       >
-        <h1 className="mb-4 text-center text-[1.5rem] font-extralight text-[var(--very-dark-blue)] md:mb-[1.2rem] md:text-4xl md:leading-[3.1rem]">
+        <h1 className="mb-4 text-center text-[1.5rem] font-extralight text-[var(--very-dark-blue)] md:text-4xl md:leading-[3.2rem]">
           Reliable, efficient delivery
           <br />
           <span className="font-semibold">Powered by Technology</span>
@@ -33,9 +33,9 @@ export default function Page() {
           Our Artificial Intelligence powered tools use millions of project data
           points to ensure that your project is successful
         </p>
-        <div className="mt-20 flex max-w-xs flex-col gap-[1.6rem] md:mt-16 md:max-w-none md:flex-row md:items-center">
-          <section className="grid basis-1/3 rounded-md border-t-4 border-t-[var(--cyan)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)]">
-            <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.5rem]">
+        <div className="mt-20 flex max-w-xs flex-col gap-[1.6rem] md:mt-[4.1rem] md:max-w-none md:flex-row md:items-center md:gap-[1.85rem]">
+          <section className="grid basis-1/3 rounded-md border-t-4 border-t-[var(--cyan)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)] md:px-8 md:pb-8">
+            <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.55rem]">
               Supervisor
             </h2>
             <p className="text-[0.8rem] leading-[1.4rem]">
@@ -46,12 +46,12 @@ export default function Page() {
               height={64}
               src={`${BASE_PATH}/four-card-feature-section/icon-supervisor.svg`}
               alt="Supervisor"
-              className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[3.2rem]"
+              className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[2.55rem] md:w-[64px]"
             />
           </section>
-          <div className="flex basis-1/3 flex-col gap-[1.6rem]">
-            <section className="grid rounded-md border-t-4 border-t-[var(--red)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)]">
-              <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.5rem]">
+          <div className="flex basis-1/3 flex-col gap-[1.6rem] md:gap-[1.85rem]">
+            <section className="grid rounded-md border-t-4 border-t-[var(--red)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)] md:px-8 md:pb-8">
+              <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.55rem]">
                 Team Builder
               </h2>
               <p className="text-[0.8rem] leading-[1.4rem]">
@@ -63,11 +63,11 @@ export default function Page() {
                 height={64}
                 src={`${BASE_PATH}/four-card-feature-section/icon-team-builder.svg`}
                 alt="Team builder"
-                className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[3.2rem]"
+                className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[2.55rem] md:w-[64px]"
               />
             </section>
-            <section className="grid rounded-md border-t-4 border-t-[var(--orange)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)]">
-              <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.5rem]">
+            <section className="grid rounded-md border-t-4 border-t-[var(--orange)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)] md:px-8 md:pb-8">
+              <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.55rem]">
                 Karma
               </h2>
               <p className="text-[0.8rem] leading-[1.4rem]">
@@ -78,12 +78,12 @@ export default function Page() {
                 height={64}
                 src={`${BASE_PATH}/four-card-feature-section/icon-karma.svg`}
                 alt="Karma"
-                className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[3.2rem]"
+                className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[2.55rem] md:w-[64px]"
               />
             </section>
           </div>
-          <section className="grid basis-1/3 rounded-md border-t-4 border-t-[var(--blue)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)]">
-            <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.5rem]">
+          <section className="grid basis-1/3 rounded-md border-t-4 border-t-[var(--blue)] bg-white p-7 shadow-[0_12px_12px_0_rgba(16,16,148,0.11)] md:px-8 md:pb-8">
+            <h2 className="mb-[0.2rem] text-xl font-semibold text-[var(--very-dark-blue)] md:mb-[0.55rem]">
               Calculator
             </h2>
             <p className="text-[0.8rem] leading-[1.4rem]">
@@ -94,7 +94,7 @@ export default function Page() {
               height={64}
               src={`${BASE_PATH}/four-card-feature-section/icon-calculator.svg`}
               alt="Calculator"
-              className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[3.2rem]"
+              className="mt-8 h-auto w-[58px] self-end justify-self-end md:mt-[2.55rem] md:w-[64px]"
             />
           </section>
         </div>

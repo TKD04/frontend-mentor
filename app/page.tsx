6a0a3f21afb2cf60dcf4c8ab1959e7c3b4e42903
TKD04 <tkd04fordev@outlook.com>
2025-05-22 19:57:49 +0900

    Move "frontendMentorLSolutionLinks.ts" to the .\constants directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import frontendMentorSolutionsLinks from "@/lib/frontendMentorSolutionsLinks";
+import frontendMentorSolutionsLinks from "@/constants/frontendMentorSolutionsLinks";
 import { ExternalLink } from "lucide-react";
 import { Inter } from "next/font/google";
 import Link from "next/link";

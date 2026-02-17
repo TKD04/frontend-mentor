3c1814c0b0053d96358746c9df94e0aa09e33727
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-02-17 19:03:14 +0900

    chore(config): remove settings injected by `actions/configure-pages`

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,3 @@
 import type { NextConfig } from "next";
 
-const GITHUB_PAGES_BASE = "/frontend-mentor";
-
-export default {
-  basePath: GITHUB_PAGES_BASE,
-  images: {
-    unoptimized: true,
-  },
-  output: "export",
-} satisfies NextConfig;
+export default {} satisfies NextConfig;

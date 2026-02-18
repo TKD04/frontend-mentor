1b6875627440da6b4e273116f4fdc8c5cbb18f79
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-02-18 21:53:13 +0900

    refactor: remove GSAP animation from accordion

--- app/faq-accordion/accordion-item.tsx
+++ app/faq-accordion/accordion-item.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 import Image from "next/image";
-import type { JSX, KeyboardEvent, MouseEvent } from "react";
-import { useEffect, useRef, useState } from "react";
+import type { JSX } from "react";
+import { useState } from "react";
 
 import minusIcon from "@/public/faq-accordion/icon-minus.svg";
 import plusIcon from "@/public/faq-accordion/icon-plus.svg";
@@ -15,92 +13,20 @@ interface AccordionItemProps {
   readonly title: string;
 }
 
-const animateOpenAccordion = (content: gsap.TweenTarget) =>
-  gsap.fromTo(
-    content,
-    {
-      height: 0,
-      opacity: 0,
-    },
-    {
-      duration: 0.4,
-      ease: "power3.out",
-      height: "auto",
-      opacity: 1,
-    },
-  );
-const animateCloseAccordion = (
-  content: gsap.TweenTarget,
-  element: HTMLDetailsElement,
-) =>
-  gsap.to(content, {
-    duration: 0.4,
-    ease: "power3.out",
-    height: 0,
-    onComplete: () => {
-      element.removeAttribute("open");
-    },
-    opacity: 0,
-    overwrite: true,
-  });
-
 const AccordionItem = ({
   content,
   isOpenDefault,
   title,
 }: AccordionItemProps): JSX.Element => {
   const [isOpen, setIsOpen] = useState(isOpenDefault);
-  const liReference = useRef<HTMLLIElement>(null);
-  const detailsReference = useRef<HTMLDetailsElement>(null);
-  const divReference = useRef<HTMLDivElement>(null);
-  const { contextSafe } = useGSAP({ scope: liReference });
-
-  const handleClickItem = (event_: MouseEvent) => {
-    event_.preventDefault();
-    setIsOpen(!isOpen);
-  };
-  const handleKeyDownItem = (event_: KeyboardEvent<HTMLElement>) => {
-    if (!(event_.key === " " || event_.key === "Enter")) {
-      return;
-    }
-
-    event_.preventDefault();
-    setIsOpen(!isOpen);
-  };
-
-  useEffect(() => {
-    contextSafe(() => {
-      const detailsCurrent = detailsReference.current;
-      const divCurrent = divReference.current;
-
-      if (detailsCurrent === null || divCurrent === null) {
-        throw new Error("detailsCurrent and divCurrent must not null");
-      }
-      if (isOpen) {
-        detailsCurrent.setAttribute("open", "true");
-        animateOpenAccordion(divCurrent).restart();
-      } else {
-        animateCloseAccordion(divCurrent, detailsCurrent).restart();
-      }
-    })();
-  }, [contextSafe, isOpen]);
 
   return (
-    <li
-      className="border-b border-b-(--light-pink) py-5 first:pt-0 last:border-b-0 last:pb-0 lg:py-6"
-      ref={liReference}
-    >
-      <details ref={detailsReference}>
-        <summary
-          // Using "flex" to remove details-marker */}
-          className="flex cursor-pointer items-center justify-between gap-4 leading-[1.2rem] font-semibold transition-colors hover:text-(--accordion-item-hover) lg:text-lg"
-          onClick={handleClickItem}
-          // Using "onKeyDown" to fix "jsx-a11y/click-events-have-key-events"
-          onKeyDown={handleKeyDownItem}
-          // Using "role" and "tabIndex" to fix "jsx-a11y/no-static-element-interactions"
-          role="button"
-          tabIndex={0}
-        >
+    <li className="border-b border-b-(--light-pink) py-5 first:pt-0 last:border-b-0 last:pb-0 lg:py-6">
+      <details
+        onToggle={(event_) => setIsOpen(event_.currentTarget.open)}
+        open={isOpen}
+      >
+        <summary className="flex cursor-pointer items-center justify-between gap-4 leading-[1.2rem] font-semibold transition-colors hover:text-(--accordion-item-hover) lg:text-lg">
           {title}
           {isOpen ? (
             <Image
@@ -124,10 +50,7 @@ const AccordionItem = ({
         </summary>
         {/* Wrapping with <div> to fix the awkward animation
             made by setting padding to the tag directly under the details */}
-        <div
-          className="overflow-hidden text-sm leading-[1.3rem] text-(--grayish-purple) lg:text-[16px] lg:leading-6"
-          ref={divReference}
-        >
+        <div className="overflow-hidden text-sm leading-[1.3rem] text-(--grayish-purple) lg:text-[16px] lg:leading-6">
           <p className="pt-6">{content}</p>
         </div>
       </details>

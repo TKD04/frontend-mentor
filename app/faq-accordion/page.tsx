43063287cef13447d70f43b22af4b58842bbd196
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-02-18 20:42:19 +0900

    refactor: switch background image breakpoint to `md`

--- app/faq-accordion/page.tsx
+++ app/faq-accordion/page.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 
 const Page = (): JSX.Element => (
   <main
-    className={`${style["colors"]} ${workSans.className} grid min-h-screen place-items-center bg-[url(/frontend-mentor/faq-accordion/background-pattern-mobile.svg)] bg-contain bg-fixed bg-top bg-no-repeat p-6 text-(--dark-purple) lg:bg-[url(/frontend-mentor/faq-accordion/background-pattern-desktop.svg)]`}
+    className={`${style["colors"]} ${workSans.className} grid min-h-screen place-items-center bg-[url(/frontend-mentor/faq-accordion/background-pattern-mobile.svg)] bg-contain bg-fixed bg-top bg-no-repeat p-6 text-(--dark-purple) md:bg-[url(/frontend-mentor/faq-accordion/background-pattern-desktop.svg)]`}
   >
     <article className="flex w-full max-w-150 flex-col gap-4 rounded-lg bg-(--white) p-6 shadow-[0_8px_24px_0_rgba(0,0,0,0.04)] lg:rounded-2xl lg:p-10 lg:shadow-[0_16px_24px_2px_rgba(0,0,0,0.06)]">
       <div className="-mt-2 mb-1.5 flex gap-6">

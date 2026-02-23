88624d29d4483360eea02b5cf6e83dd32ef971dc
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-02-23 15:26:51 +0900

    refactor: update responsive break points and refine styling

--- app/faq-accordion/accordion-item.tsx
+++ app/faq-accordion/accordion-item.tsx
@@ -19,40 +19,29 @@ const AccordionItem = ({
   isOpen,
   item,
 }: AccordionItemProps): JSX.Element => (
-  <li className="border-b border-b-(--light-pink) py-5 first:pt-0 last:border-b-0 last:pb-0 lg:py-6">
+  <li className="border-b border-b-(--light-pink) py-5 first:pt-0 last:border-b-0 last:pb-0 md:py-6">
     <details open={isOpen}>
       <summary
-        className="flex cursor-pointer items-center justify-between gap-4 leading-[1.2rem] font-semibold transition-colors hover:text-(--accordion-item-hover) lg:text-lg"
+        className="flex cursor-pointer items-center justify-between gap-4 leading-[1.2rem] font-semibold transition-colors hover:text-(--accordion-item-hover) md:text-lg"
         onClick={(event_) => {
           event_.preventDefault();
           handleClick(index);
         }}
       >
         {item.title}
-        {isOpen ? (
-          <Image
-            alt="Minus"
-            aria-hidden
-            className="h-auto w-7.5"
-            height={31}
-            src={minusIcon}
-            width={30}
-          />
-        ) : (
-          <Image
-            alt="Plus"
-            aria-hidden
-            className="h-auto w-7.5"
-            height={31}
-            src={plusIcon}
-            width={30}
-          />
-        )}
+        <Image
+          alt={isOpen ? "Minus" : "Plus"}
+          aria-hidden
+          className="h-auto w-7.5"
+          height={31}
+          src={isOpen ? minusIcon : plusIcon}
+          width={30}
+        />
       </summary>
-      {/* Wrapping with <div> to fix the awkward animation
-            made by setting padding to the tag directly under the details */}
-      <div className="overflow-hidden text-sm leading-[1.3rem] text-(--grayish-purple) lg:text-[16px] lg:leading-6">
-        <p className="pt-6">{item.content}</p>
+      <div className="overflow-hidden">
+        <p className="pt-6 text-sm leading-[1.3rem] text-(--grayish-purple) md:text-[16px] md:leading-6">
+          {item.content}
+        </p>
       </div>
     </details>
   </li>
--- app/faq-accordion/page.tsx
+++ app/faq-accordion/page.tsx
@@ -26,19 +26,19 @@ export const metadata: Metadata = {
 
 const Page = (): JSX.Element => (
   <main
-    className={`${style["colors"]} ${workSans.className} grid min-h-screen place-items-center bg-[url(/frontend-mentor/faq-accordion/background-pattern-mobile.svg)] bg-contain bg-fixed bg-top bg-no-repeat p-6 text-(--dark-purple) md:bg-[url(/frontend-mentor/faq-accordion/background-pattern-desktop.svg)]`}
+    className={`${style["colors"]} ${workSans.className} grid min-h-screen place-items-center bg-(--light-pink) bg-[url(/frontend-mentor/faq-accordion/background-pattern-mobile.svg)] bg-contain bg-fixed bg-top bg-repeat-x p-6 text-(--dark-purple) md:bg-[url(/frontend-mentor/faq-accordion/background-pattern-desktop.svg)]`}
   >
-    <article className="flex w-full max-w-150 flex-col gap-4 rounded-lg bg-(--white) p-6 shadow-[0_8px_24px_0_rgba(0,0,0,0.04)] lg:rounded-2xl lg:p-10 lg:shadow-[0_16px_24px_2px_rgba(0,0,0,0.06)]">
-      <div className="-mt-2 mb-1.5 flex gap-6">
+    <article className="flex w-full max-w-150 flex-col rounded-lg bg-(--white) p-6 pt-5 shadow-[0_8px_24px_0_rgba(0,0,0,0.04)] md:rounded-2xl md:p-10 md:pt-7.5 md:shadow-[0_16px_24px_2px_rgba(0,0,0,0.08)]">
+      <div className="mb-4 flex gap-6 md:mb-5">
         <Image
           alt="Star"
           aria-hidden
-          className="h-auto w-6 lg:w-9"
+          className="h-auto w-6 md:w-10"
           height={41}
           src={starIcon}
           width={40}
         />
-        <h1 className="text-[2rem] font-bold lg:text-[3.6rem]">FAQs</h1>
+        <h1 className="text-[2rem] font-bold md:text-[3.6rem]">FAQs</h1>
       </div>
       <Accordion items={FAQS_ACCORDION_ITEMS} />
     </article>
